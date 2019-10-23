c5b97655fc01660be67d1c95acc647c12bb83e93
신호태 <ubiquitous6666@gmail.com>
2019-10-23 16:20:55 +0900

    19.10.23

--- react/mar_notes/src/queries.js
+++ react/mar_notes/src/queries.js
@@ -1,20 +1,20 @@
- import gql from "graphql-tag";
- import { NOTE_FRAGMENT } from "./fragments";
+import gql from "graphql-tag";
+import { NOTE_FRAGMENT } from "./fragments";
 
- export const GET_NOTES = gql`
-    {
-        notes @client {
-            ...NoteParts
-        }
+export const GET_NOTES = gql`
+  {
+    notes @client {
+      ...NoteParts
     }
-    ${NOTE_FRAGMENT}
- `;
+  }
+  ${NOTE_FRAGMENT}
+`;
 
- export const GET_NOTE = gql`
-    query getNote($id: Int!) {
-        note(id: $id) @client {
-            ...NoteParts
-        }
+export const GET_NOTE = gql`
+  query getNote($id: Int!) {
+    note(id: $id) @client {
+      ...NoteParts
     }
-    ${NOTE_FRAGMENT}
+  }
+  ${NOTE_FRAGMENT}
 `;
